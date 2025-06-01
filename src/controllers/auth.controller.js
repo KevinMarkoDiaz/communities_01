c1e16e3aaed0f8c9b7e816b0de8ae8c26eea133e
kev dia <kevindi0695@gmail.com>
2025-06-01 02:16:16 -0500

    secure true

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -121,7 +121,7 @@ export const loginUser = async (req, res) => {
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
+          secure: true,//process.env.NODE_ENV === "production", cambiar cuando deploye front
           sameSite: "Lax",
           maxAge: 3600000,
         })
