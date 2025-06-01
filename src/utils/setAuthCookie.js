57ba05bbbdd53fc1c6258365e74ae71595dd3df6
kev dia <kevindi0695@gmail.com>
2025-06-01 13:11:57 -0500

    secure true

--- src/utils/setAuthCookie.js
+++ src/utils/setAuthCookie.js
@@ -1,11 +1,10 @@
 // src/utils/setAuthCookie.js
 export function setAuthCookie(res, token) {
-  const isProd = process.env.NODE_ENV === "production";
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: isProd, // true en producción
-    sameSite: isProd ? "None" : "Lax",
+    secure: true, // true en producción
+    sameSite: "None",
     maxAge: 3600000, // 1h
   });
 }
