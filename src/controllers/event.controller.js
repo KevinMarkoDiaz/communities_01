a117cf6128ec7c48f435f723c97c96052bebec2b
kev dia <kevindi0695@gmail.com>
2025-06-02 13:09:11 -0500

    add my events route-controller

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -1,4 +1,6 @@
 import Event from "../models/event.model.js";
+import User from "../models/user.model.js";
+import Business from "../models/business.model.js";
 
 export const createEvent = async (req, res) => {
   const {
